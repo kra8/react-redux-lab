55a769f3f69068d5f44a0704e4db5437c3e7809d
K.Asai <me@kra.dev>
2019-05-24 16:47:31 +0900

    tweak

--- Vanilla/App.js
+++ Vanilla/App.js
@@ -1,7 +1,26 @@
 import React from 'react'
 
+const data = [
+  { id: 1, body: '#ticket 1' },
+  { id: 2, body: '#ticket 2' },
+  { id: 3, body: '#ticket 3' }
+]
+
 const App = () => {
-  return <div>Hello Vanilla</div>
+  return (
+    <div>
+      <select>
+        <option value='1'>value1</option>
+        <option value='2'>value2</option>
+      </select>
+      <input />
+      <ul>
+        {data.map(todo => (
+          <li key={todo.id}>{todo.body}</li>
+        ))}
+      </ul>
+    </div>
+  )
 }
 
 export default App
